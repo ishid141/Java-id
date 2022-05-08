cafe3ff8c8bcfa6a60f3f27659a5a4b61bda62bf
ishid141 <ishid141@gmail.com>
2022-05-08 12:24:27 -0500

    Updated HelloWorld

---
+++ Helloworld/src/com/ishika/Main.java
@@ -0,0 +1,32 @@
+package com.ishika;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //practice for primitive values
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        //Reference Types
+        byte egg = 30;
+        Date now = new Date ();
+        System.out.println(now);
+
+        //Difference between Primitive and Reference
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+
+        //Strings
+        String message = "Hello World" + "!!";
+        System.out.println(message);
+
+    }
+}
